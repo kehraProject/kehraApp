946d1b1ba6d893d8fe20198e612028f5c9df1d02
Claudia Vitolo <claudia.vitolo@gmail.com>
2016-06-30 07:56:04 +0100

    Added 239 active air pollution stations

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ library(rgdal)
 library(googleVis)
 
 shinyServer(function(input, output, session) {
-  
+
   dataset1 <- reactive({df1 <- bbox[bbox$Name == input$location, ]})
 
   ## Interactive Map ###########################################################
@@ -17,19 +17,19 @@ shinyServer(function(input, output, session) {
       # addTiles(group = "OSM (default)") %>%
       addProviderTiles("Esri.NatGeoWorldMap", group = "NatGeoWorldMap") %>%
       addProviderTiles("Esri.WorldStreetMap", group = "Streets") %>%
-      setView(lng = df1[,"longmid"], 
-              lat = df1[,"latmid"], 
+      setView(lng = df1[,"longmid"],
+              lat = df1[,"latmid"],
               zoom=df1[,"zoomlevel"]) %>%
       # Overlay groups #
       addPolygons(data=adm0, weight=2, group = "Study Areas") %>%
-      addPolygons(data=adm1, weight=2, 
+      addPolygons(data=adm1, weight=2,
                   group = "Admin Boundaries 1", col="yellow") %>%
       # addPolygons(data=adm2, weight=2, group = "Admin Boundaries 2", col="orange") %>%
       addPolygons(data=AralSea, weight=2, col="gray", group = "Aral Sea (late 1950s)") %>%
-      addCircles(lng = nuclearSites$Lon, 
-                 lat = nuclearSites$Lat, 
-                 fillColor = "yellow", color = "blue",  
-                 weight = 3, radius = 10000, 
+      addCircles(lng = nuclearSites$Lon,
+                 lat = nuclearSites$Lat,
+                 fillColor = "yellow", color = "blue",
+                 weight = 3, radius = 10000,
                  popup = paste(#"<b>Country:</b>", nuclearSites$Country, "<br>",
                                #"<b>Location:</b>", nuclearSites$Location,"<br>",
                                "<b>Site:</b>", nuclearSites$Site, "<br>",
@@ -37,63 +37,78 @@ shinyServer(function(input, output, session) {
                                #"<b>Longitude:</b>", nuclearSites$Lon, "<br>",
                                "<b>Description:</b>", nuclearSites$Description),
                  group = "Nuclear sites") %>%
-      addCircles(lng = SleepingTowns$Longitude, 
-                 lat = SleepingTowns$Latitude, 
-                 fillColor = "red", color = "red", 
-                 weight = 3, radius = 20000, 
+      addCircles(lng = SleepingTowns$Longitude,
+                 lat = SleepingTowns$Latitude,
+                 fillColor = "red", color = "red",
+                 weight = 3, radius = 20000,
                  popup = paste("<b>Town:</b>", SleepingTowns$Towns, "<br>",
                                "<b>Description:</b>", SleepingTowns$Reason),
                  group = "Sleeping Towns") %>%
-      addCircles(lng = mineralResources$Longitude, 
-                 lat = mineralResources$Latitude, 
-                 fillColor = "brown", color = "brown", 
-                 weight = 3, radius = 10000, 
+      addCircles(lng = mineralResources$Longitude,
+                 lat = mineralResources$Latitude,
+                 fillColor = "brown", color = "brown",
+                 weight = 3, radius = 10000,
                  popup = paste("<b>Site name:</b>", mineralResources$SITE_NAME,
                                "<br>",
                                "<b>Description:</b>", mineralResources$URL),
                  group = "Mineral Resource Data System") %>%
-      addCircles(lng = monitoredCities$LONGITUDE, 
+      addCircles(lng = monitoredCities$LONGITUDE,
                  lat = monitoredCities$LATITUDE,
-                 fillColor = "red", color = "red", weight = 3, 
-                 radius = ifelse(monitoredCities$POP_MAX > 1, 
+                 fillColor = "red", color = "red", weight = 3,
+                 radius = ifelse(monitoredCities$POP_MAX > 1,
                                  sqrt(abs(monitoredCities$POP_MAX))*30, 0),
                  group = "Populated places") %>%
-      addCircles(lng = populatedPlaces$LONGITUDE, 
+      addCircles(lng = populatedPlaces$LONGITUDE,
                  lat = populatedPlaces$LATITUDE,
-                 fillColor = "orange", color = "orange", weight = 3, 
-                 radius = ifelse(populatedPlaces$POP_MAX > 1, 
-                                 sqrt(abs(populatedPlaces$POP_MAX))*30, 0), 
+                 fillColor = "orange", color = "orange", weight = 3,
+                 radius = ifelse(populatedPlaces$POP_MAX > 1,
+                                 sqrt(abs(populatedPlaces$POP_MAX))*30, 0),
                  popup = paste("<b>",populatedPlaces$NAME, "</b><br>",
                                "Max population:", populatedPlaces$POP_MAX),
                  group = "Populated places") %>%
+      addCircles(lng = meteostationsKZ$Longitude,
+                 lat = meteostationsKZ$Latitude,
+                 fillColor = "magenta", color = "magenta",
+                 weight = 3, radius = 10000,
+                 popup = paste("<b>",
+                               "Station name:", meteostationsKZ$Meteost_name,
+                               "</b><br>",
+                               "Oblast:", meteostationsKZ$Adm_obl,
+                               "</b><br>",
+                               "Region:", meteostationsKZ$Adm_region,
+                               "</b><br>",
+                               "Altitude:", meteostationsKZ$Altitude),
+                 group = "Meteostations") %>%
       # Layers control #
       addLayersControl(
         position = 'bottomleft',
         baseGroups = c("NatGeoWorldMap", "Streets"),
-        overlayGroups = c("Study Areas", 
-                          "Admin Boundaries 1", # "Admin Boundaries 2", 
+        overlayGroups = c("Study Areas",
+                          "Admin Boundaries 1", # "Admin Boundaries 2",
                           "Aral Sea (late 1950s)",
                           "Nuclear sites",
                           "Sleeping Towns",
                           "Populated places",
-                          "Mineral Resource Data System"),
+                          "Mineral Resource Data System",
+                          "Meteostations"),
         #overlayGroups = c("Nuclear sites", "AWS", "Health Risk Spots"),
         options = layersControlOptions(collapsed = TRUE)
       ) %>%
       hideGroup(c("Nuclear sites", "Populated places" ,
                   "Admin Boundaries 1", #"Admin Boundaries 2"
-                  "Aral Sea (late 1950s)", "Sleeping Towns", 
-                  "Mineral Resource Data System"
+                  "Aral Sea (late 1950s)", "Sleeping Towns",
+                  "Mineral Resource Data System",
+                  "Meteostations"
                   ))
   })
-  
+
   ## Data explorer #############################################################
-  
+
   output$plot1 <- renderPlot({
-    
+
     VI <- readRDS("data/VulnerabilityIndex.rds")
-    
-    plot(VI$VulnerabilityI ~ VI$Ranking, 
+
+    plot(VI$VulnerabilityI ~ VI$Ranking,
          main = "KZ and UK are not amongst the most vulnerable countries to climate change",
          xlab="Ranking", ylab="Vulnerability Index")
     KZ <- VI[VI$Sovereign=="Kazakhstan",]
@@ -101,42 +116,42 @@ shinyServer(function(input, output, session) {
     UK <- VI[VI$Sovereign=="U.K.",]
     points(UK$VulnerabilityI ~ UK$Ranking, col="blue", pch=20, cex= 1.5)
     legend("topleft", pch = 20, col=c("red","blue"), legend=c("KZ","UK"))
-    
+
   })
-  
+
 #   ## Project's timeline ########################################################
-#   
+#
 #   output$timeline <- renderGvis({
-#     
-#     datTL <- data.frame(Name=c("Admin", "Data Collection", "Model Building", 
-#                                "Model Testing", "Model Debugging", 
-#                                "Paper 1", "Paper 2", "Paper 3", 
+#
+#     datTL <- data.frame(Name=c("Admin", "Data Collection", "Model Building",
+#                                "Model Testing", "Model Debugging",
+#                                "Paper 1", "Paper 2", "Paper 3",
 #                                "Reporting", "Workshop"),
 #                         Task=c("Andrew", "Allan", "Claudia",
 #                                "Abraham", "Maged",
 #                                "Aygul", "Sara", "Kanat", "Yelzhas", "Bella"),
 #                         start=as.Date(x=c("2015-05-01", "2015-05-01",
-#                                           "2015-10-01", "2015-10-01", 
-#                                           "2015-10-01", "2015-10-01", 
-#                                           "2015-10-01", "2015-10-01", 
+#                                           "2015-10-01", "2015-10-01",
+#                                           "2015-10-01", "2015-10-01",
+#                                           "2015-10-01", "2015-10-01",
 #                                           "2015-10-01", "2015-10-01")),
-#                         end=as.Date(x=c("2017-03-31", "2017-03-31", 
-#                                         "2017-03-31", "2017-03-31", 
-#                                         "2017-03-31", "2017-03-31", 
-#                                         "2017-03-31", "2017-03-31", 
+#                         end=as.Date(x=c("2017-03-31", "2017-03-31",
+#                                         "2017-03-31", "2017-03-31",
+#                                         "2017-03-31", "2017-03-31",
+#                                         "2017-03-31", "2017-03-31",
 #                                         "2017-03-31", "2017-03-31")))
-#     
-#     gvisTimeline(data=datTL, 
+#
+#     gvisTimeline(data=datTL,
 #                  rowlabel="Name",
 #                  barlabel="Task",
-#                  start="start", 
+#                  start="start",
 #                  end="end",
 #                  options=list(timeline="{groupByRowLabel:false}",
-#                               backgroundColor='#ffd', 
+#                               backgroundColor='#ffd',
 #                               height=7500#,
 #                               #colors="['#cbb69d', '#603913', '#c69c6e']"
 #                               ))
-#     
+#
 #   })
 
 })
